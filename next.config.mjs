4c1ee86bbb084b3dc1dd38c79fb97fda4d358822
MADHU-RELLI <enoshpaulrelli@gmail.com>
2025-10-28 12:37:07 +0530

    v2

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  distDir: 'dist',
   images: {
     unoptimized: true,
   },
